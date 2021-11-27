769db3ea5adc6b814bc4867fd8519ae8782cb6fb
izerozlu <izerozlu@gmail.com>
2021-11-27 11:26:54 +0300

    Add store to the application

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { useStore } from "./store";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const store = useStore();
+
+app.use(store);
+app.mount("#app");
